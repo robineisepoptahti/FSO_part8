7234462c0091936ef7b5eea69e19fa978caafa10
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2025-05-04 00:28:03 +0300

    Added 8.4

--- library-backend.js
+++ library-backend.js
@@ -113,7 +113,7 @@ type Author {
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks: [Book!]
+    allBooks(author: String): [Book!]
     allAuthors: [Author!]
   }
 `;
@@ -122,7 +122,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      } else {
+        return books.filter((book) => book.author === args.author);
+      }
+    },
     allAuthors: () => authors,
   },
   Author: {
